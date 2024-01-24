0873934ee4c4dd50dfec904c8a9f1c3c02c88924
pavan kumar <siriyalapavankumar.com>
2024-01-24 07:19:58 +0530

    4/18✅

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,6 @@ app.use("/api/v1", mainRouter);
 
 // /api/v1/user/signup
 // /api/v1/user/signin
-// /api/v1/user/changePassword ....
+// /api/v1/user/changePassword 
 // /api/v1/account/transferMoney
 // /api/v1/account/balance
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const zod= require("zod")
+
 
 const router = express.Router();
 
-// singnup secheme here
 
- const signUpScheme=
 
 module.exports = router;
