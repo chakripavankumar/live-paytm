760a2f75e7a708ea19eb99ee4d7244475cdf7dab
pavan kumar <siriyalapavankumar@gmail.com>
2024-01-24 12:20:06 +0530

    7/18✅

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,14 @@
 const express = require("express");
+
+const router = express.Router();
 const zod=require("zod");
 const {User} = require("../db")
 const jwt= require("jsonwebtoken");
 const {JWT_SECRET}= require("../config")
 
-const router = express.Router();
 
-const signupSchema= zod.object({
+
+const signupBody= zod.object({
     username:zod.string(),
     passwords:zod.string(),
     fristName:zod.string(),
@@ -14,7 +16,7 @@ const signupSchema= zod.object({
 })
 router.post("/signup", async(req,res)=>{
     const body= req.body;
-    const  {success} = signupSchema.safeParse(req.body);
+    const  {success} = signupBody.safeParse(req.body);
     if(!success){
         return res.status(411).json({
             message: "Email already taken / Incorrect inputs"
@@ -43,7 +45,35 @@ router.post("/signup", async(req,res)=>{
         message:"user created sucessfully",
         token:token
     })
+})
 
+const signinBody= zod.object({
+    username:zod.string().email(),
+    passsword:zod.string()
+})
+router.post("/signin" , async(req,res)=>{
+    const {success}= signinBody.safeParse(req.body)
+    if(!success){
+        return res.status(411).json({
+            message: "incorrect inputs"
+        });
+    }
+     const user = await User.findOne({
+        username:req.body.username,
+        passsword:req.body.passsword
+     });
+     if(user){
+        const token= jwt.sign({
+            userId:user._id
+        },JWT_SECRET);
+        res.json({
+         token:token
+        })
+        return;
+     }
+     res.status(411).json({
+        message:"Error while logging in"
+     })
 })
 
 
