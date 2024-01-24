2931b649e37b6d712abb0b8c3ecee5a8e2de6ece
pavan kumar <siriyalapavankumar@gmail.com>
2024-01-24 11:15:53 +0530

    6/18✅

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,6 +6,46 @@ const {JWT_SECRET}= require("../config")
 
 const router = express.Router();
 
+const signupSchema= zod.object({
+    username:zod.string(),
+    passwords:zod.string(),
+    fristName:zod.string(),
+    lastName:zod.string(),
+})
+router.post("/signup", async(req,res)=>{
+    const body= req.body;
+    const  {success} = signupSchema.safeParse(req.body);
+    if(!success){
+        return res.status(411).json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+ const existinguser=  await User.findOne({
+    username:body.username
+ })
+ if(existinguser){
+    return res.status(411).json({
+        message: "Email already taken / Incorrect inputs"
+    })
+ }
+    const user= await User.create({
+        username:req.body.username,
+        passsword:req.body.passsword,
+        fristName:req.body.fristName,
+        lastName:req.body.lastName,
+    })
+    const userId= user._id;
+
+    const token= jwt.sign({
+        userId
+    },JWT_SECRET)
+    res.json({
+        message:"user created sucessfully",
+        token:token
+    })
+
+})
+
 
 
 module.exports = router;
